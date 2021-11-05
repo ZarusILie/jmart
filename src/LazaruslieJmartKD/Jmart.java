9073043b0f9766940e751899a7e3e3b1ff6cc76a
ZarusILie <lazarusliekarsono@gmail.com>
2021-11-05 13:43:58 +0700

    TP modul6 Lazaruslie Karsono

--- src/LazaruslieJmartKD/Jmart.java
+++ src/LazaruslieJmartKD/Jmart.java
@@ -1,15 +1,39 @@
 package LazaruslieJmartKD;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 /**
  * Jmart
  *
  * @author (Lazaruslie Karsono)
  */
-public class Jmart 
-{    
-    public static void main(String[] args) 
-    {
+class Jmart
+{
+    class Country {
+        public String name;
+        public int population;
+        public List<String> listOfStates;
+    }
+
+    public static void main(String[] args) {
+        String filepath = "D:\\OOP\\jmart\\lib\\city.json";
+        Gson gson = new Gson();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filepath));
+            Country input = gson.fromJson(br, Country.class);
+            System.out.println("name: " + input.name);
+            System.out.println("population: " + input.population);
+            System.out.println("states:");
+            input.listOfStates.forEach(state -> System.out.println(state));
+        }
+
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
         //Product productSnack = createProduct();
         //Coupon couponDiskon = createCoupon();
         //Shipment shipment = createShipment();
