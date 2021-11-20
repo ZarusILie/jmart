e1cc609cc36c14c37ea42b502112b38e77cf9bac
ZarusILie <lazarusliekarsono@gmail.com>
2021-11-20 11:12:16 +0700

    CS modul8 Lazaruslie Karsono

--- LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/AccountController.java
+++ LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/AccountController.java
@@ -1,5 +1,7 @@
 package com.LazaruslieJmartKD.controller;
 import com.LazaruslieJmartKD.Account;
+import com.LazaruslieJmartKD.Store;
+import com.LazaruslieJmartKD.dbjson.JsonAutowired;
 import com.LazaruslieJmartKD.dbjson.JsonTable;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,25 +10,22 @@ import java.util.regex.Pattern;
 @RestController
 @RequestMapping("/account")
 
-public class AccountController {
+public class AccountController implements BasicGetController<Account> {
 
-    /*
     public static final String REGEX_EMAIL = "^\\w+([\\.&_*~]?\\w+)*@\\w+([\\.\\-]?\\w+)*(\\.\\w{2,3})+$";
-    public static final String REGEX_PASSWORD = "";
-    public static final Pattern REGEX_PATTERN_EMAIL = ;
-    public static final Pattern REGEX_PATTERN_PASSWORD = ;
-    public static JsonTable<Account> accountTable;
+    public static final String REGEX_PASSWORD = "^(?=.[a-z])(?=.[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$";
+    public static final Pattern REGEX_PATTERN_EMAIL = Pattern.compile(REGEX_EMAIL);
+    public static final Pattern REGEX_PATTERN_PASSWORD = Pattern.compile(REGEX_EMAIL);
+    public static @JsonAutowired(value=Account.class, filepath="src/main") JsonTable<Account> accountTable;
 
     public JsonTable<Account> getJsonTable() {
-
+        return accountTable;
     }
-
-     */
 
     @GetMapping
     String index() { return "account page"; }
 
-    /*
+
     @PostMapping("/login")
     Account login
             (
@@ -34,10 +33,13 @@ public class AccountController {
                     @RequestParam String password
             )
     {
-        return new Account(email, password);
+        for(Account account : accountTable) {
+            if(account.email.equals(email) && account.password.equals(password)) {
+                return account;
+            }
+        }
+        return null;
     }
-
-     */
 
     @PostMapping("/register")
     Account register
@@ -47,11 +49,47 @@ public class AccountController {
                     @RequestParam String password
             )
     {
-        return new Account(name, email, password, 0);
+        if((REGEX_PATTERN_EMAIL.matcher(email).find()) && (REGEX_PATTERN_PASSWORD.matcher(password).find()) && !name.isBlank()) {
+            for(Account account : accountTable){
+                if(account.email.equals(email)){
+                    return null;
+                }
+            }
+            return new Account(name, email, password, 0);
+        }
+        return null;
+    }
+    /*
+    @PostMapping("/{id}/registerStore")
+    Store registerStore(int id, double balance, String name, String address, String phoneNumber) {
+        if(accountTable.contains(accountTable.get(id)) && accountTable.get(id).store == null) {
+            Store newStore = new Store(name, address, phoneNumber, 0);
+            accountTable.get(id).store = newStore;
+            return newStore;
+        }
+        else{
+            return null;
+        }
     }
 
+     */
+
+    @PostMapping("/{id}/topUp")
+    boolean topUp(int id, double balance){
+        if(accountTable.contains(accountTable.get(id))) {
+            accountTable.get(id).balance += balance;
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+    /*
     @GetMapping("/{id}")
     String getById(@PathVariable int id) {
         return "account id " + id + " not found!";
     }
+     */
 }
---
+++ LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/PaymentController.java
@@ -0,0 +1,22 @@
+package com.LazaruslieJmartKD.controller;
+
+import com.LazaruslieJmartKD.ObjectPoolThread;
+import com.LazaruslieJmartKD.Payment;
+import com.LazaruslieJmartKD.dbjson.JsonTable;
+
+public class PaymentController {
+
+    /*
+    public static final long DELIVERED_LIMIT_MS;
+    public static final long ON_DELIVERY_LIMIT_MS;
+    public static final long ON_PROGRESS_LIMIT_MS;
+    public static final long WAITING_CONF_LIMIT_MS;
+    public static JsonTable<Payment> paymentTable;
+    public static ObjectPoolThread<Payment> poolThread;
+
+    boolean accept (int id) {
+        return false;
+    }
+     */
+
+}
