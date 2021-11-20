69add31c276e3648ccdfdcdaa60690ef1a53b69e
ZarusILie <lazarusliekarsono@gmail.com>
2021-11-20 10:42:52 +0700

    CS modul8 Lazaruslie Karsono

--- LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/BasicGetController.java
+++ LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/BasicGetController.java
@@ -9,14 +9,14 @@ public interface BasicGetController<T extends Serializable> {
 
     @GetMapping("/id")
     default T getById(int id) {
-        return Algorithm.<T>find(getJsonTable(), (e) -> e.id==id);
+        return Algorithm.<T> find(getJsonTable(), (e) -> e.id == id);
     }
 
     JsonTable<T> getJsonTable();
 
     @GetMapping("/page")
     default List<T> getPage(int page, int pageSize) {
-        return Algorithm.<T>paginate(getJsonTable(), page, pageSize, e->true);
+        return Algorithm.<T> paginate(getJsonTable(), page, pageSize, e -> true);
     }
 
 }
