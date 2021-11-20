bf0be3b49cc49f578b25ab66437e182bb7de8751
ZarusILie <lazarusliekarsono@gmail.com>
2021-11-20 10:40:54 +0700

    CS modul8 Lazaruslie Karsono

--- LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/AccountController.java
+++ LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/AccountController.java
@@ -1,13 +1,43 @@
 package com.LazaruslieJmartKD.controller;
 import com.LazaruslieJmartKD.Account;
+import com.LazaruslieJmartKD.dbjson.JsonTable;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/account")
 
 public class AccountController {
+
+    /*
+    public static final String REGEX_EMAIL = "^\\w+([\\.&_*~]?\\w+)*@\\w+([\\.\\-]?\\w+)*(\\.\\w{2,3})+$";
+    public static final String REGEX_PASSWORD = "";
+    public static final Pattern REGEX_PATTERN_EMAIL = ;
+    public static final Pattern REGEX_PATTERN_PASSWORD = ;
+    public static JsonTable<Account> accountTable;
+
+    public JsonTable<Account> getJsonTable() {
+
+    }
+
+     */
+
     @GetMapping
     String index() { return "account page"; }
+
+    /*
+    @PostMapping("/login")
+    Account login
+            (
+                    @RequestParam String email,
+                    @RequestParam String password
+            )
+    {
+        return new Account(email, password);
+    }
+
+     */
 
     @PostMapping("/register")
     Account register
---
+++ LazaruslieJmartKD/src/main/java/com/LazaruslieJmartKD/controller/BasicGetController.java
@@ -0,0 +1,23 @@
+package com.LazaruslieJmartKD.controller;
+import com.LazaruslieJmartKD.Algorithm;
+import com.LazaruslieJmartKD.dbjson.JsonTable;
+import com.LazaruslieJmartKD.dbjson.Serializable;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+public interface BasicGetController<T extends Serializable> {
+
+    @GetMapping("/id")
+    default T getById(int id) {
+        return Algorithm.<T>find(getJsonTable(), (e) -> e.id==id);
+    }
+
+    JsonTable<T> getJsonTable();
+
+    @GetMapping("/page")
+    default List<T> getPage(int page, int pageSize) {
+        return Algorithm.<T>paginate(getJsonTable(), page, pageSize, e->true);
+    }
+
+}
+
